6bac79c70a7d11a554b14c2ba4a23e2e155fcd11
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-05-09 17:14:40 +0000

    index.py

---
+++ api/v1/views/index.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""retrieves the number of each objects by type"""
+
+
+from flask import jsonify
+from models import storage
+from api.v1.views import api.views
+
+
+@app_views.route('/status')
+def api_status():
+    """returns a JSON: "status": "OK" """
+    return flask.jsonify(status='OK')
+
+
+@app_views.route('/status')
+def api_stat():
+    """retrieves the number of each objects by type"""
+    return jsonify({
+                    "amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")
+                  })
