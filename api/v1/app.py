32655f862612b493ee1230419dfadcf861b326c7
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-05-09 17:39:03 +0000

    error 404 handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,6 +11,11 @@ from os import getenv
 app = Flask(__name__, instance_relative_config=True)
 app.register_blueprint(app_views)
 
+@app.errorhandler(404)
+def page_not_found(error):
+    """returns a JSON-formatted 404 status code response."""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 if __name__ == "__main__":
     app.run(
         host=getenv('HBNB_API_HOST', '0.0.0.0'),
