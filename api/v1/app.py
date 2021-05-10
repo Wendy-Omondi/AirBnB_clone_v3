245babe3ba9010902f5193269ab1c6283e66d9c5
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-05-10 18:46:57 +0000

    start api

---
+++ api/v1/app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""flask application"""
+
+
+from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
+from models import storage
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Closes storage session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """returns a JSON-formatted 404 status code response."""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+if __name__ == "__main__":
+    app.run(
+        host=getenv('HBNB_API_HOST', '0.0.0.0'),
+        port=getenv('HBNB_API_PORT', 5000),
+        threaded=True
+    )
---
+++ api/v1/views/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""initilalizes the api"""
+
+
+from flask import Blueprint
+
+app_views = Blueprint('api', __name__, url_prefix='/api/v1')
+
+
+from api.v1.views.index import *
