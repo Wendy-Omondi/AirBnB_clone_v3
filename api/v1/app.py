4f29feb90ec71a10785a306cf00a4d757f29a998
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-05-10 14:22:47 +0000

    correct import error for api_views

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,12 @@
 """flask application"""
 
 
-import app.views from api.v1.views
+from api.v1.views import app_views
 from flask import Flask, make_response, jsonifyimport
-from flask_cors import CORS
 from models import storage
 from os import getenv
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.errorhandler(404)
